382715b51ed4da9aa977136731ef0eee2c46bbff
HemyDev <97851451+Elezthem@users.noreply.github.com>
2023-08-25 23:23:08 +0300

    Add files via upload

---
+++ Ijunior/Program.cs
@@ -0,0 +1,7 @@
+﻿string stringNumber = "256";
+
+int numberFromString = Convert.ToInt32(stringNumber);
+
+int square = numberFromString * numberFromString;
+
+Console.WriteLine("Квадрат введенного числа равен " + square);
